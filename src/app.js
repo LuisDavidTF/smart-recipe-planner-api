533191895959cc6736b424f0a9c275dc28cf1346
LuisDavidTF <luisdavidtf55@gmail.com>
2025-10-27 11:09:59 -0600

    feat(cors): Implement CORS to configure allowed origins in the API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import { jsonSyntaxErrorHandler } from '#middlewares/jsonSyntaxErrorHandler.js'; // Importamos el middleware para manejar errores de sintaxis JSON
+import cors from 'cors';
 import userRoutes from '#api/auth/auth.routes.js'; // Importamos nuestras rutas de usuario
 import recipeRoutes from '#api/recipes/recipes.routes.js'; // Importamos nuestras rutas de recetas
 import errorHandler from '#middlewares/errorHandler.js';// Importamos el middleware de manejo de errores
@@ -8,6 +9,28 @@ import errorHandler from '#middlewares/errorHandler.js';// Importamos el middlew
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configuramos CORS
+app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "https://*.scf.usercontent.goog"
+]; // Añade los orígenes permitidos aquí
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 // Middleware para que Express entienda JSON
 app.use(json());
