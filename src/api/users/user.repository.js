96967135859a05ffba4add9c5d531b64036ee929
LuisDavidTF <luisdavidtf55@gmail.com>
2025-11-03 11:46:40 -0600

    refactor(user.repository): Improve the documentation of user-related functions. feat(user.repository): Add the function (gerUserById) to retrieve the user by their ID.

--- src/api/users/user.repository.js
+++ src/api/users/user.repository.js
@@ -1,12 +1,39 @@
 import prisma from '#config/prisma.js';
+import { create } from '../recipes/recipes.repository';
 
-// Repositorio para crear un nuevo usuario
+/**
+ * Crea un nuevo usuario
+ * @param {*} data 
+ * @returns {Promise<import('@prisma/client').User>} Usuario creado
+ */
 export async function createUser(data) {
   return await prisma.user.create({ data });// Creamos un nuevo usuario en la base de datos y retornamos el usuario creado
 }
 
-// Repositorio para loguear un usuario por su email
-// Retorna el usuario encontrado o null si no existe
+/**
+ * Inicia sesión un usuario por su email
+ * @param {*} email 
+ * @returns {Promise<import('@prisma/client').User | null>} Usuario encontrado o null si no existe
+ */
 export async function loginUser(email) {
   return await prisma.user.findUnique({ where: { email } });// Buscamos un usuario por su email y retornamos el usuario encontrado
+}
+
+/**
+ * Obtiene un usuario por su ID
+ * @param {*} id 
+ * @returns {Promise<import('@prisma/client').User | null>} Usuario encontrado o null si no existe
+ */
+export async function getUserById(id) {
+  return await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      profile_picture_url: true,
+      role: true,
+      createdAt: true
+    }
+  });// Buscamos un usuario por su ID y retornamos el usuario encontrado
 }
